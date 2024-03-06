2a667b21b265ae722785b1e83740cadb8c6b73bc
bmallar <benmallar@gmail.com>
2024-03-05 19:55:42 -0600

    add structure, add footer

---
+++ assets/js/form.js
@@ -0,0 +1,10 @@
+const back = document
+const back = 
+
+// Clicking previous displays previous image in carousel
+back.addEventListener('click', function (event) {
+    // Event bubbling would occur and a new window would open if we did not include the following line of code.
+    event.stopPropagation();
+  
+    navigate(-1);
+  });
