b6eb9994584a94082b738b55da9459fcc430d761
bmallar <benmallar@gmail.com>
2024-03-05 18:54:19 -0600

    update for ask BCS

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -4,32 +4,32 @@ const blogForm = document.getElementById('blog-form');
 let isDark = true;
 
 themeButtonEl.on('click', function () {
-     if (isDark) {
-      $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
-      isDark = !isDark;
+    if (isDark) {
+        $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
+        isDark = !isDark;
     } else {
-      $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
-      isDark = !isDark;
+        $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
+        isDark = !isDark;
     }
-  });
+});
 
 
 
-  blogForm.addEventListener('submit', function(event){
+blogForm.addEventListener('submit', function (event) {
     event.preventDefault();
-   const postArray = JSON.parse(localStorage.getItem('postlist'))|| []
-   
+    const postArray = JSON.parse(localStorage.getItem('postlist')) || []
+
     const blogUsername = document.getElementById('username').value.trim();
     const blogTitle = document.getElementById('title').value.trim();
     const blogContent = document.getElementById('content').value.trim();
 
-  const blogObject = {
-    username: blogUsername,
-    title: blogTitle,
-    content: blogContent
- }
- 
- postArray.push(blogObject)
- localStorage.setItem('postlist', JSON.stringify(postArray))
- console.log(blogObject)
+    const blogObject = {
+        username: blogUsername,
+        title: blogTitle,
+        content: blogContent
+    }
+
+    postArray.push(blogObject)
+    localStorage.setItem('postlist', JSON.stringify(postArray))
+    console.log(blogObject)
 })
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,17 +7,17 @@ function displayPosts() {
         blogCard.setAttribute("class", "blogCard")
 
         const usernameEl = document.createElement("h4")
-       usernameEl.textContent = blogArray[i].username
-        
+        usernameEl.textContent = blogArray[i].username
+
         const titelEl = document.createElement('h4')
         titelEl.textContent = blogArray[i].title
 
         const contentEl = document.createElement('h4')
         contentEl.textContent = blogArray[i].content
-  
-        blogCard.append(usernameEl, titelEl,contentEl)
+
+        blogCard.append(usernameEl, titelEl, contentEl)
         postContainer.append(blogCard)
-  
+
     }
 }
 
